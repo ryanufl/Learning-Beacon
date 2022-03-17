5edd3e48a559819908780ea5d92e29b9227fbfd4
ryanufl <rpallack@ufl.edu>
2022-03-17 17:03:13 -0400

    small work for main menu

--- testSWEProject/testSWEProject.cpp
+++ testSWEProject/testSWEProject.cpp
@@ -5,9 +5,26 @@
 using namespace std;
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode(1600, 1000), "Learning Beacon", sf::Style::Close);
+
+
+    sf::RectangleShape background(sf::Vector2f(1600, 1000));
+    sf::Texture fishtankBackground;
+    fishtankBackground.loadFromFile("fishtank.jpg");
+    background.setTexture(&fishtankBackground);
+
+
+    sf::RectangleShape playButton(sf::Vector2f(400, 150));
+    sf::RectangleShape exitButton(sf::Vector2f(400, 150));
+    
+    playButton.setPosition(600,300);
+    exitButton.setPosition(600,500);
+    
+    playButton.setOutlineColor(sf::Color::Black);
+    playButton.setOutlineThickness(5);
+
+    exitButton.setOutlineColor(sf::Color::Black);
+    exitButton.setOutlineThickness(5);
 
     while (window.isOpen())
     {
@@ -16,14 +33,25 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::MouseButtonPressed) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    //cout << event.mouseButton.x << "   :X\n\ny: " << event.mouseButton.y << "\n";
+                    if (playButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                        playButton.setFillColor(sf::Color::Yellow);
+                    }else if(exitButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                        window.close();
+                    }
+                }
+            }
         }
 
         window.clear();
-        window.draw(shape);
+        window.draw(background);
+        window.draw(playButton);
+        window.draw(exitButton);
         window.display();
     }
 
-    cout << "Hello World! NNCEBLUIWEBFO";
 
     return 0;
 }
