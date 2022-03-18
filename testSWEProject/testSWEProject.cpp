1ef9e8f94467c30f842c8e5e2137016f9d8e8ea8
ryanufl <rpallack@ufl.edu>
2022-03-18 15:39:06 -0400

    added support for more than one screen

--- testSWEProject/testSWEProject.cpp
+++ testSWEProject/testSWEProject.cpp
@@ -1,8 +1,11 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <vector>
+#include "Screen.h"
 
 
 using namespace std;
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(1600, 1000), "Learning Beacon", sf::Style::Close);
@@ -11,7 +14,7 @@ int main()
     sf::RectangleShape background(sf::Vector2f(1600, 1000));
     sf::Texture fishtankBackground;
     fishtankBackground.loadFromFile("fishtank.jpg");
-    background.setTexture(&fishtankBackground);
+    background.setTexture(&fishtankBackground); 
 
 
     sf::RectangleShape playButton(sf::Vector2f(400, 150));
@@ -26,6 +29,22 @@ int main()
     exitButton.setOutlineColor(sf::Color::Black);
     exitButton.setOutlineThickness(5);
 
+    vector<Screen*> screens;
+
+    Screen startMenu(true);
+    screens.push_back(&startMenu);
+    startMenu.thingsToDraw.emplace("background", background);
+    startMenu.thingsToDraw.emplace("play", playButton);
+    startMenu.thingsToDraw.emplace("exit", exitButton);
+
+    Screen mathGame(false);
+    screens.push_back(&mathGame);
+    mathGame.thingsToDraw.emplace("background", background);
+    mathGame.thingsToDraw.find("background")->second.setFillColor(sf::Color(40, 40, 40));
+    mathGame.thingsToDraw.emplace("backButton", sf::RectangleShape(sf::Vector2f(500, 500)));
+    mathGame.thingsToDraw.find("backButton")->second.setPosition(400, 400);
+
+
     while (window.isOpen())
     {
         sf::Event event;
@@ -36,19 +55,30 @@ int main()
             if (event.type == sf::Event::MouseButtonPressed) {
                 if (event.mouseButton.button == sf::Mouse::Left) {
                     //cout << event.mouseButton.x << "   :X\n\ny: " << event.mouseButton.y << "\n";
-                    if (playButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
+                    if (startMenu.thingsToDraw.find("play")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && startMenu.needToDraw) {
                         playButton.setFillColor(sf::Color::Yellow);
-                    }else if(exitButton.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y)) {
-                        window.close();
+                        //background.setFillColor(sf::Color(40, 40, 40));
+                        startMenu.thingsToDraw.find("play")->second.setFillColor(sf::Color::Yellow);
+                    }else if(startMenu.thingsToDraw.find("exit")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && startMenu.needToDraw) {
+                        //window.close();
+                        startMenu.needToDraw = false;
+                        mathGame.needToDraw = true;
+                    }
+                    else if (mathGame.thingsToDraw.find("backButton")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && mathGame.needToDraw) {
+                        //window.close();
+                        startMenu.needToDraw = true;
+                        mathGame.needToDraw = false;
                     }
                 }
             }
         }
 
         window.clear();
-        window.draw(background);
-        window.draw(playButton);
-        window.draw(exitButton);
+        for (int i = 0; i < screens.size(); i++) {
+            if (screens.at(i)->needToDraw) {
+                screens.at(i)->showScreen(window);
+            }
+        }
         window.display();
     }
 
