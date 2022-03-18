665c4b65683a78cad2068d94f2799067a41051ac
ryanufl <rpallack@ufl.edu>
2022-03-18 15:41:46 -0400

    added comments

--- testSWEProject/testSWEProject.cpp
+++ testSWEProject/testSWEProject.cpp
@@ -8,41 +8,41 @@ using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1600, 1000), "Learning Beacon", sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(1600, 1000), "Learning Beacon", sf::Style::Close); //creates intial window
 
 
-    sf::RectangleShape background(sf::Vector2f(1600, 1000));
-    sf::Texture fishtankBackground;
-    fishtankBackground.loadFromFile("fishtank.jpg");
-    background.setTexture(&fishtankBackground); 
+    sf::RectangleShape background(sf::Vector2f(1600, 1000)); //adds background
+    sf::Texture fishtankBackground;                          //
+    fishtankBackground.loadFromFile("fishtank.jpg");         //sets up background 
+    background.setTexture(&fishtankBackground);              //
 
 
-    sf::RectangleShape playButton(sf::Vector2f(400, 150));
-    sf::RectangleShape exitButton(sf::Vector2f(400, 150));
-    
-    playButton.setPosition(600,300);
-    exitButton.setPosition(600,500);
-    
-    playButton.setOutlineColor(sf::Color::Black);
-    playButton.setOutlineThickness(5);
+    sf::RectangleShape playButton(sf::Vector2f(400, 150));   //
+    sf::RectangleShape exitButton(sf::Vector2f(400, 150));   //
+                                                             //
+    playButton.setPosition(600,300);                         //
+    exitButton.setPosition(600,500);                         //creates two main menu buttons
+                                                             //moves them
+    playButton.setOutlineColor(sf::Color::Black);            //
+    playButton.setOutlineThickness(5);                       //
+                                                             //
+    exitButton.setOutlineColor(sf::Color::Black);            //
+    exitButton.setOutlineThickness(5);                       //
 
-    exitButton.setOutlineColor(sf::Color::Black);
-    exitButton.setOutlineThickness(5);
+    vector<Screen*> screens;                                               //used to correctly print one screen at a time
 
-    vector<Screen*> screens;
+    Screen startMenu(true);                                               //
+    screens.push_back(&startMenu);                                        //
+    startMenu.thingsToDraw.emplace("background", background);             //adds all buttons/images used for startMenu ========WILL NEED TO CHANGE WHEN USING SPRITES========
+    startMenu.thingsToDraw.emplace("play", playButton);                   //
+    startMenu.thingsToDraw.emplace("exit", exitButton);                   //
 
-    Screen startMenu(true);
-    screens.push_back(&startMenu);
-    startMenu.thingsToDraw.emplace("background", background);
-    startMenu.thingsToDraw.emplace("play", playButton);
-    startMenu.thingsToDraw.emplace("exit", exitButton);
-
-    Screen mathGame(false);
-    screens.push_back(&mathGame);
-    mathGame.thingsToDraw.emplace("background", background);
-    mathGame.thingsToDraw.find("background")->second.setFillColor(sf::Color(40, 40, 40));
-    mathGame.thingsToDraw.emplace("backButton", sf::RectangleShape(sf::Vector2f(500, 500)));
-    mathGame.thingsToDraw.find("backButton")->second.setPosition(400, 400);
+    Screen mathGame(false);                                                                     //
+    screens.push_back(&mathGame);                                                               //
+    mathGame.thingsToDraw.emplace("background", background);                                    //
+    mathGame.thingsToDraw.find("background")->second.setFillColor(sf::Color(40, 40, 40));       // where math game UI will be
+    mathGame.thingsToDraw.emplace("backButton", sf::RectangleShape(sf::Vector2f(500, 500)));    //
+    mathGame.thingsToDraw.find("backButton")->second.setPosition(400, 400);                     //
 
 
     while (window.isOpen())
@@ -54,18 +54,14 @@ int main()
                 window.close();
             if (event.type == sf::Event::MouseButtonPressed) {
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    //cout << event.mouseButton.x << "   :X\n\ny: " << event.mouseButton.y << "\n";
-                    if (startMenu.thingsToDraw.find("play")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && startMenu.needToDraw) {
-                        playButton.setFillColor(sf::Color::Yellow);
-                        //background.setFillColor(sf::Color(40, 40, 40));
+                    if (startMenu.thingsToDraw.find("play")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && startMenu.needToDraw) {//tests bounds of button and if the screen is currently visable
+                        //playButton.setFillColor(sf::Color::Yellow); //setting color can be helpful for confirming an input happened
                         startMenu.thingsToDraw.find("play")->second.setFillColor(sf::Color::Yellow);
-                    }else if(startMenu.thingsToDraw.find("exit")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && startMenu.needToDraw) {
-                        //window.close();
+                    }else if(startMenu.thingsToDraw.find("exit")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && startMenu.needToDraw) {//tests bounds of button and if the screen is currently visable
                         startMenu.needToDraw = false;
                         mathGame.needToDraw = true;
                     }
-                    else if (mathGame.thingsToDraw.find("backButton")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && mathGame.needToDraw) {
-                        //window.close();
+                    else if (mathGame.thingsToDraw.find("backButton")->second.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y) && mathGame.needToDraw) {//tests bounds of button and if the screen is currently visable
                         startMenu.needToDraw = true;
                         mathGame.needToDraw = false;
                     }
